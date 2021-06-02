ad50c1e7a067bff243efc34a0417306110308f04
ahmedTrainning <trainning7007@gmail.com>
2021-06-02 18:47:06 +0300

    calury

---
+++ calorieCalculatorProgram/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace calorieCalculatorProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(171, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "welcome to caluries calculator \r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PS : with no exercizes done .\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "calculater";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 240);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ calorieCalculatorProgram/Form2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace calorieCalculatorProgram
+{
+    
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tb1, tb2, tb3;
+            double.TryParse(textBox1.Text, out tb1);
+            double.TryParse(textBox2.Text, out tb2);
+            double.TryParse(textBox3.Text, out tb3);
+            
+            if (((Regex.IsMatch(textBox1.Text, @"^\d+$"))&& (Regex.IsMatch(textBox2.Text, @"^\d+$"))&&(Regex.IsMatch(textBox3.Text, @"^\d+$"))) ==true)
+            {
+                if (checkBox1.Checked && !checkBox2.Checked)
+                {
+                    double men = 66 + (6.2 * tb1) + (12.7 * tb2) - (6.67 * tb3);
+                    MessageBox.Show("you are burnning " + men + " everyday without exercize");
+
+                }
+                else if (checkBox2.Checked && !checkBox1.Checked)
+                {
+                    double women = 655.1 + (4.35 * tb1) + (4.7 * tb2) - (4.7 * tb3);
+                    MessageBox.Show("you are burnning " + women + " everyday without exercize");
+                }
+                else if (checkBox1.Checked && checkBox2.Checked)
+                {
+                    MessageBox.Show("pls choose only men or women ");
+                }
+                else
+                {
+                    MessageBox.Show("pls choose gender");
+                }
+            }
+            else if (((Regex.IsMatch(textBox1.Text, @"^\d+$")) && (Regex.IsMatch(textBox2.Text, @"^\d+$")) && (Regex.IsMatch(textBox3.Text, @"^\d+$"))) == false)
+
+            { 
+                MessageBox.Show("you can only put numbers");
+            }
+            else
+            {
+                MessageBox.Show("pls insert the data");
+            }
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 fm1 = new Form1();
+            fm1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            if ((checkBox1.Checked && checkBox2.Checked)==true)
+            {
+                checkBox1.Checked = !checkBox1.Checked;
+                checkBox2.Checked = !checkBox2.Checked;
+            }
+            else
+            {
+                checkBox1.Checked = !checkBox1.Checked;
+                checkBox2.Checked = !checkBox2.Checked;
+            }
+
+        }
+    }
+}
